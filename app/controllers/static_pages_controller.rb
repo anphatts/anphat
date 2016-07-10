fac2bac054b7736f792d4b4e0d5c7ac80f367bc2
tript <trongtri92nd@gmail.com>
2016-07-10 21:31:22 +0700

    fix bug: "PDT not display lesson uploaded by its own user" (homepage)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,8 +14,13 @@ class StaticPagesController < ApplicationController
       @type.each do |type|
         type_lesson = TypeLesson.new
         type_lesson.type = type
-        quantity = Lesson.joins(:user).where(users: {school_id: @school.id}, type_id: type.id).size
-        lessons = Lesson.joins(:user).where(users: {school_id: @school.id}, type_id: type.id).limit(10)
+        if (@school.grade_id == 5)
+          quantity = Lesson.joins(user: :school).where("schools.pdt_id = ? OR schools.id = ?", @school.id, @school.id).where(type_id: type.id).size
+          lessons = Lesson.joins(user: :school).where("schools.pdt_id = ? OR schools.id = ?", @school.id, @school.id).where(type_id: type.id).limit(10)
+        else
+          quantity = Lesson.joins(:user).where(users: {school_id: @school.id}, type_id: type.id).size
+          lessons = Lesson.joins(:user).where(users: {school_id: @school.id}, type_id: type.id).limit(10)
+        end
         type_lesson.quantity = quantity
         type_lesson.lessons = lessons
         @type_lessons.push(type_lesson)
